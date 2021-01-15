34844a5cfdf786de315673886f2b56f22bc5bc88
Eduardo Henrique <eduardohds.dev@outlook.com>
2021-01-15 15:56:57 -0300

    Building server routes from config.json

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 [![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/eduhds/json-server-test)
 
+# JSON-SERVER-TEST
+
+## Usage
+
+-   Edite o arquivo `config.json` adicionando os nomes das rotas:
+
+```json
+{
+	"gets": ["test-get", ...],
+	"posts": ["test-post", ...]
+}
+```
+
+-   Para cada nome de rota, adicione um arquivo JSON de mesmo nome no diretório `json`
+
+```
+json/
+    test-get.json
+    test-post.json
+    ...
+```
+
+-   Start with Node
+
+```bash
+npm run start
+```
+
+-   Access on `localhost:3030`
--- index.js
+++ index.js
@@ -7,21 +7,35 @@ const port = 3030;
 app.use(express.json());
 
 app.all('/', (req, res) => {
-    res.send('Hello!');
+	res.send('Hello!');
 });
 
 app.listen(port, () =>
-    console.log(`Server running on ${ip.address()}:${port}...`)
+	console.log(`Server running on ${ip.address()}:${port}...`)
 );
 
 app.on('error', erro => {
-    console.log('ERRO', erro);
+	console.log('ERRO', erro);
 });
 
 //===========================================
 //                   ROUTES
 //===========================================
 
-app.post('/ROUTE', (req, res) => {
-    res.json({})
-});
+const config = require('./config.json');
+
+for (const get of config.gets) {
+	app.get(`/${get}`, (req, res) => {
+		console.log(`/${get} Body:`, req.body);
+		const json = require(`./json/${get}.json`);
+		res.json(json);
+	});
+}
+
+for (const post of config.posts) {
+	app.get(`/${post}`, (req, res) => {
+		console.log(`/${post} Body:`, req.body);
+		const json = require(`./json/${post}.json`);
+		res.json(json);
+	});
+}
